74dbb84d006e5e5d82cc9bd1f22bbfc879e89a17
Erik Peterson <Erik.Exists@gmail.com>
2012-10-18 10:59:13 -0400

    Added a linear/delta rule, basic testing testing suggests it is working.

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 """ A top-level testing script. """
 import numpy as np
 from annclass.misc import setup_bias
-from annclass.update import perceptron
+from annclass.update import perceptron_update, linear_update
+from annclass.activation import linear
 
 
 def print_perceptron_update(xs, ws, bias, y):
@@ -10,15 +11,32 @@ def print_perceptron_update(xs, ws, bias, y):
     # Convert xs ans ws to the proper form
     # for use in perceptron
     xs, ws = setup_bias(xs, ws, bias)
-    ws_new = perceptron(xs, ws, y)
+    ws_new, h = perceptron_update(xs, ws, y)
 
     print("ws_intial: {0}\nws_new: {1}".format(ws, ws_new))
 
+
+def print_linear_update(xs, ws, bias, y, ep):
+    """ Prints initial and updated ws, and h. """
+    
+    # Convert xs ans ws to the proper form
+    # for use in perceptron
+    xs = np.array(xs)
+    ws = np.array(ws) 
+        # Not using setup_bias so do this by hand.
+    
+    ws_new, h = linear_update(xs, ws, y, ep)
+    h_new = linear(xs, ws_new, None)
+    
+    print("ws_intial: {0}\nws_new: {1}".format(ws, ws_new))
+    print("h_intial: {0}\nh_new: {1}".format(h,h_new))
+    
 
 if __name__ == "__main__":
     
     # ----
     # Lecture 2.1 quiz:
+    print("----\nLecture 2.1 quiz:\n----")
     xs = [0.5, -0.5]
     ws = [2, -1]
     bias = 0.5
@@ -26,6 +44,24 @@ if __name__ == "__main__":
 
     print_perceptron_update(xs, ws, bias, y)
     print("ws_new should be [-0.5, 1.5, -0.5]")
+        ## correct quiz answer of 
+        ##   ws_new is [1.5, -0.5] with a bias of -0.5
+        ## suggests perceptron code is working
+        ## correctly ....so far anyway
+    # ----
+
+    # ----
+    # Lecture 3.1 quiz:
+    print("----\nLecture 3.1 quiz:\n----")
+    xs = [2, 5, 3]
+    ws = [50, 50, 50]
+    bias = None
+    y = 850
+    ep = 1/35.
+
+    print_linear_update(xs, ws, bias, y, ep)
+    print("ws_new should be [70, 100, 80]")
+    print("h_new should be 880")
         ## correct quiz answer of 
         ##   ws_new is [1.5, -0.5] with a bias of -0.5
         ## suggests perceptron code is working
--- update.py
+++ update.py
@@ -1,9 +1,9 @@
 """ Weight update rules. """
 import numpy as np
-from annclass.activation import binary
+from annclass.activation import binary, linear
 
 
-def perceptron(xs, ws, y):
+def perceptron_update(xs, ws, y):
     """ The proven good perceptron learning rule. """
     
     if y not in set((0,1)):
@@ -13,7 +13,7 @@ def perceptron(xs, ws, y):
     ws = np.array(ws)
         ## Explicit typing just in case
        
-    act = binary(xs, ws, None)
+    h = binary(xs, ws, None)
 
     # ----
     # If they're not equal update and 
@@ -22,19 +22,49 @@ def perceptron(xs, ws, y):
     # The update rule:
     # If target is 0 when act is 1, add xs to ws
     # If target is 1 when act is 0, subtract xs from ws
-    if act != y:
-        if act == 0:
+    if h != y:
+        if h == 0:
             ## if act and y are not the same,
             ## then is act is 0 y must be 1....
-            ws = ws + xs
-        elif act == 1:
+            ws_new = ws + xs
+        elif h == 1:
             ## and the reverse as well.
-            ws = ws - xs
+            ws_new = ws - xs
         else:
             raise ValueError(
-                    "activation (binary()) or target (y) values are illegal.")
+                    "activation (h) or target (y) values are illegal.")
             ## just in case, shouldn't get hit
         
-    return ws
+    return ws_new, h
         ## If act and y were equal, 
         ## ws gets returned unchanged.
+
+
+def linear_update(xs, ws, y, ep):
+    """ Update rules for linear ANNs, use the delta rule to update the weights,
+    i.e. ep * xs (h - y), where h is the linear activation 
+    (from annclass.activation.linear).  
+    
+    Note: in multilayer ANN an average of two good weights may be a bad 
+    set of weights, i.e. they are not convex.  Unlike perceptrons, whose
+    weights are proven to reach a 'good' set of weights, linear (or multilayer)
+    ANNs are proven instead to have their predictions approach the target. Also 
+    note that perceptrons are not proven to have their predictions approach the 
+    target, yet some how the weights are known-good.  Odd.
+    
+    FYI, Linear neurons are linear filters in EE (with ep being the severity
+    of filtration).  For linear cases and squared error functions there are of 
+    course analytical solutions for the update rather than the 
+    iterative procedure used here (e.g. OLS).  Iterative methods are used 
+    so non-linear functions and non-squared error functions can be used, 
+    and to better mimic the brain.
+    """
+    
+    h = linear(xs, ws, None)
+    print(xs*(h-y))
+    delta_w = float(ep) * (xs * (y - h))
+    print(delta_w)
+    ws_new = ws + delta_w
+    
+    return ws_new, h
+
